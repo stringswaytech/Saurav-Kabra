7519c90385e807451c1bd0761c70112bb4001375
stringswaytech <53994155+stringswaytech@users.noreply.github.com>
2019-09-08 00:00:05 +0530

    Add files via upload

---
+++ String/home.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+
+	$('.card').hover(
+		function(){
+			$(this).animate({
+				marginTop: "-=1%",
+			},200);
+		},
+		function(){
+			$(this).animate({
+				marginTop: "0%",
+			},200);
+		}
+
+		);
+});
